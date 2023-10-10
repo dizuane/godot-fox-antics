1d34184ab5a6cfce1a2d8b1d2c4530b60d831aea
Duane Spainhour <duane.spainhour@gmail.com>
2023-10-10 14:21:22 -0400

    Setup initial player, player animations, and camera

---
+++ level_base/level_base.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var player_cam = $PlayerCam
+@onready var player = $Player
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	player_cam.position = player.position
