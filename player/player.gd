172b0e1e52960b66b18e0d2d20440f1fbe6f82cb
Duane Spainhour <duane.spainhour@gmail.com>
2023-10-11 01:12:58 -0400

    Added logic for hurt player state

--- player/player.gd
+++ player/player.gd
@@ -4,19 +4,23 @@ class_name Player
 @onready var sprite_2d = $Sprite2D
 @onready var animation_player = $AnimationPlayer
 @onready var shooter = $Shooter
+@onready var animation_player_invincible = $AnimationPlayerInvincible
+@onready var hurt_timer = $HurtTimer
 
 const GRAVITY: float = 1000.0
 const RUN_SPEED: float = 120.0
 const MAX_FALL: float = 400.0
-const HURT_TIME: float = 0.3
 const JUMP_VELOCITY: float = -400.0
+const HURT_JUMP_VELOCITY: Vector2 = Vector2(0, -150.0)
 
 enum PLAYER_STATE { IDLE, RUN, JUMP, FALL, HURT }
 
 @onready var debug_label = $DebugLabel
 @onready var sound_player = $SoundPlayer
+@onready var invincible_timer = $InvincibleTimer
 
 var _state: PLAYER_STATE = PLAYER_STATE.IDLE
+var _invincible: bool = false
 
 func _ready():
 	pass
@@ -36,10 +40,11 @@ func _physics_process(delta):
 
 	
 func update_debug_label() -> void:
-	debug_label.text = "floor: %s\n%s\nvelocity x: %.0f\nvelocity y: %.0f" % [
+	debug_label.text = "floor: %s\n%s\nvelocity x: %.0f\nvelocity y: %.0f\ninvincible: %s" % [
 		is_on_floor(),
 		PLAYER_STATE.keys()[_state],
-		velocity.x, velocity.y
+		velocity.x, velocity.y,
+		_invincible
 	]
 
 
@@ -51,6 +56,9 @@ func shoot() -> void:
 
 
 func get_input() -> void:
+	if _state == PLAYER_STATE.HURT:
+		return
+
 	velocity.x = 0
 	
 	if Input.is_action_pressed("left"):
@@ -102,5 +110,36 @@ func set_state(new_state: PLAYER_STATE) -> void:
 			animation_player.play("fall")
 
 
+func go_invincible() -> void:
+	_invincible = true
+	animation_player_invincible.play("invincible")
+	invincible_timer.start()
+
+
+func apply_hurt_jump() -> void:
+	set_state(PLAYER_STATE.HURT)
+	animation_player.play("hurt")
+	velocity = HURT_JUMP_VELOCITY
+	hurt_timer.start()
+
+
+func apply_hit() -> void:
+	if _invincible:
+		return
+	
+	go_invincible()
+	apply_hurt_jump()
+	SoundManager.play_clip(sound_player, SoundManager.SOUND_DAMAGE)
+
+
 func _on_hit_box_area_entered(area):
-	print("Player HitBox: ", area)
+	apply_hit()
+
+
+func _on_invincible_timer_timeout():
+	_invincible = false
+	animation_player_invincible.stop()
+
+
+func _on_hurt_timer_timeout():
+	set_state(PLAYER_STATE.IDLE)
