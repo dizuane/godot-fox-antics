99e93fab05a96a3c8db849e63eba32ede7a7a885
Duane Spainhour <duane.spainhour@gmail.com>
2023-10-11 00:52:31 -0400

    Enemies now drop fruits

--- fruit_pickup/fruit_pickup.gd
+++ fruit_pickup/fruit_pickup.gd
@@ -1,14 +1,35 @@
 extends Area2D
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
-# Called when the node enters the scene tree for the first time.
+const FRUITS: Array = [
+	"melon",
+	"kiwi",
+	"banana",
+	"cherry"
+]
+const GRAVITY: float = 250.0
+const JUMP: float = -80.0
+const POINTS: int = 2
+
+var _start_y: float
+var _speed_y: float = JUMP
+var _stopped: bool = false
+
 func _ready():
-	pass # Replace with function body.
+	_start_y = global_position.y
+	animated_sprite_2d.play(FRUITS.pick_random())
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if _stopped:
+		return
+	
+	position.y += _speed_y * delta
+	_speed_y += GRAVITY * delta
+	
+	if global_position.y > _start_y:
+		_stopped = true
 
 
 func kill_me() -> void:
@@ -18,4 +39,10 @@ func kill_me() -> void:
 
 
 func _on_life_timer_timeout():
+	kill_me()
+
+
+func _on_area_entered(area):
+	print("Pickup Collected")
+	SignalManager.on_pickup_hit.emit(POINTS)
 	kill_me()
